cb3a148b553e34bd9d4670531f2b675999b533f1
Rishabh Maru <148260712+Rishabhmaru01@users.noreply.github.com>
2025-11-15 22:05:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+        function updateTimer() {
+            const timer = document.getElementById("timer");
+            const now = new Date();
+
+            // Format date & time 
+            const formatted = now.toLocaleString(); 
+            
+            timer.textContent = formatted;
+        }
+
+        // Update immediately
+        updateTimer();
+
+        // Update every 1 second
+        setInterval(updateTimer, 1000);
